b5bb7430f3c71129f5b3842277eef18bb8955b33
hyuck <choiggoggi@gmail.com>
2019-06-06 18:31:31 +0900

    everything

---
+++ algorithm/practice.py
@@ -0,0 +1,8 @@
+def choose(start, end):
+    if start == end:
+        return print('I am here!')
+
+    for choice in range(2):
+        if choice == 0:
+            print('I walk!')
+            if start + 1 > end
