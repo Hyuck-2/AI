152353d04e00169f3bf2908300dbff3c4d2ac2b0
hyuck <choiggoggi@gmail.com>
2019-07-17 10:59:32 +0900

    final commit

--- algorithm/practice.py
+++
@@ -1,8 +0,0 @@
-def choose(start, end):
-    if start == end:
-        return print('I am here!')
-
-    for choice in range(2):
-        if choice == 0:
-            print('I walk!')
-            if start + 1 > end
